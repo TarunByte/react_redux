65e4ed8c7fe8b777346b6b3c08c37f6c1fb7b7e1
TarunBits <tarunbhattmortal@gmail.com>
2025-07-27 22:47:53 +0530

    Redux Tutorial #2 Create Reducer Functions to Add & Delete Tasks in React

---
+++ src/store.jsx
@@ -0,0 +1,28 @@
+const ADD_TASK = "task/add";
+const DELETE_TASK = "task/delete";
+
+const initialState = {
+  task: [],
+};
+
+const askReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      };
+
+    case DELETE_TASK:
+      const updatedTask = state.task.filter((curTask, index) => {
+        return index !== action.payload;
+      });
+      return {
+        ...state,
+        task: [...state.task, updatedTask],
+      };
+
+    default:
+      return state;
+  }
+};
