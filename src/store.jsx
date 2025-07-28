78bef5fb5c0966bdf50cf0a755416910bc0fa187
TarunBits <tarunbhattmortal@gmail.com>
2025-07-28 19:51:43 +0530

    Redux Store: Create, Dispatch & Get State in React

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,14 @@
 const ADD_TASK = "task/add";
 const DELETE_TASK = "task/delete";
+import { createStore } from "redux";
 
 const initialState = {
   task: [],
+  isLoading: false,
 };
 
-const askReducer = (state = initialState, action) => {
+//Step 1: Create a simple reducer function
+const taskReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TASK:
       return {
@@ -26,3 +29,22 @@ const askReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Step 2: Create the Redux store using the reducer
+const store = createStore(taskReducer);
+console.log(store);
+
+// Step 3: Log the initial state
+// The getState method is a synchronous function that returns the current state of a Redux application. It includes the entire state of the application, including all the reducers and their respective states.
+
+console.log("inital State", store.getState());
+
+// Step 4: Dispatch an action to add a task
+store.dispatch({ type: ADD_TASK, payload: "Buy ThapaTechnical Code" });
+console.log("updated State", store.getState());
+
+store.dispatch({ type: ADD_TASK, payload: "Buy Mango" });
+console.log("updated State", store.getState());
+
+store.dispatch({ type: DELETE_TASK, payload: 1 });
+console.log("deleted State", store.getState());
